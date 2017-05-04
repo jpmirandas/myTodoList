e83a24882b962c2f8cd29ece49f02b8d6e8cb2eb
João Paulo Miranda <jpmirandas@gmail.com>
2017-05-03 22:13:39 -0300

    Added field validation and css file

--- src/js/jpLib.js
+++ src/js/jpLib.js
@@ -2,7 +2,15 @@ $(function(){
 	
 	$('#bt-addItem').click(function(){
 		var text = $('[name="item"]').val();
-		console.info('Item added: ' + text);
-		$('ul').append('<li>' + text + '</li>');
+
+		if(text == ""){
+			$('[name="item"]').addClass('empty-field');
+			alert('A task can\'t be empty');
+		}else{
+			$('ul').append('<li>' + text + '</li>');
+			$('[name="item"]').removeClass('empty-field');
+			console.info('Item added: ' + text);
+		}
+		
 	});
 });
