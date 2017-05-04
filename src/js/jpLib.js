6c88617bccd8a1f7679efbde1f6d6286c63ddd7b
João Paulo Miranda <jpmirandas@gmail.com>
2017-05-03 21:25:50 -0300

    An added item is put into list

--- src/js/jpLib.js
+++ src/js/jpLib.js
@@ -1,3 +1,8 @@
 $(function(){
 	
+	$('#bt-addItem').click(function(){
+		var text = $('[name="item"]').val();
+		console.info('Item added: ' + text);
+		$('ul').append('<li>' + text + '</li>');
+	});
 });
