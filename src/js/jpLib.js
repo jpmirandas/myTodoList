0d50596249469a3b1a7650f53097d85dad841f30
João Paulo Miranda <jpmirandas@gmail.com>
2017-05-04 23:17:55 -0300

    listening the checkbox events ... BUG: the text should be replaced after checking

--- src/js/jpLib.js
+++ src/js/jpLib.js
@@ -7,10 +7,22 @@ $(function(){
 			$('[name="item"]').addClass('empty-field');
 			alert('A task can\'t be empty');
 		}else{
-			$('ul').append('<li>' + text + '</li>');
+			$('ul').append('<li><input type="checkbox" name="'+text+'">'+ text +'</li>');
+			
 			$('[name="item"]').removeClass('empty-field');
+			$('[name="item"]').val('');
 			console.info('Item added: ' + text);
 		}
 		
 	});
+
+$(document).on('change', 'input[type="checkbox"]', function() {
+    if(this.checked) {
+      var inputName = this.getAttribute('name')
+      $('li input[name="'+inputName+'"]').replaceWith('<li><input type="checkbox" name="'+inputName+'"><del>'+ inputName +'</del></li>');
+    }
+});
+
+
+
 });
